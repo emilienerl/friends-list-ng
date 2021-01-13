d11ecc32f9e91f47730ece31faee0dd4b611e7d4
Emilie Nerl <emilie.nerl1@gmail.com>
2021-01-13 18:31:32 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'friends-list';
+  newFriend = '';
+  isDisabled = false;
+  // create an array named friends and populate with some friends
+  friends = ["Sean", "Julie", "Phil", "Ron", "Dana"];
+
+  // add to your friend list
+  addFriend() {
+    this.friends.push(this.newFriend);
+  }
+ 
 }
